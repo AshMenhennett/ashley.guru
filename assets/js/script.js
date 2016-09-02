07dc2fe2244fcdd705d96310b56564ba4e631021
Ashley Menhennett <ashleymenhennett@gmail.com>
2016-09-02 21:59:39 +0930

    Commiting v0.1 files

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	$("#toggle-navigation").on('click', function(){
+		$("#main-navigation-list-container").slideToggle();
+	});
+
+	/* Smooth scroll adapted from: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* Adapted from: http://webdesignerwall.com/tutorials/animated-scroll-to-top/comment-page-3 */
+    // hide #back-top first
+    $("#back-top").hide();
+    
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+});
